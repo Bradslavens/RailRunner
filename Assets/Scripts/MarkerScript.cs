800537e27c43579bd5f1d7cbfb2f6e9a491447cd
BradSlavens <bradslavens@gmail.com>
2023-07-03 16:34:30 -0700

    added road and refractored the marker logic

--- Assets/Scripts/MarkerScript.cs
+++ Assets/Scripts/MarkerScript.cs
@@ -5,46 +5,49 @@ using UnityEngine;
 
 public class MarkerScript : MonoBehaviour
 {
-    // Marker trigger string
-    public string markerTrigger;
 
-    // Speed of marker movement
     [SerializeField]
     private float speed;
 
     public TrackTransport trackTransport;
 
-    public QuestionManager questionManager;
+    public float enableRendererPosition = 5f;
+    public float disableRendererPosition = 0f;
 
-    // Property to control if marker is movable
-    private bool isMoveable = false;
+    private MeshRenderer[] childMeshRenderers;
 
     private void Start()
     {
         speed = trackTransport.speed;
-        StartCoroutine(SetMoveable());
-    }
 
-    IEnumerator SetMoveable()
-    {
-        yield return new WaitForSeconds(3); // wait for 3 seconds
-        isMoveable = true;
+        // Disable all child mesh renderers at start, including inactive children
+        childMeshRenderers = GetComponentsInChildren<MeshRenderer>(true);
+        foreach (MeshRenderer renderer in childMeshRenderers)
+        {
+            renderer.enabled = false;
+        }
     }
 
     void Update()
     {
-        // If marker is moveable, then check if current answer equals marker trigger string
-        if (isMoveable && questionManager.correctObject.transform.GetChild(0).GetComponent<TextMeshPro>().text == markerTrigger)
-        {
-            // Move this marker backwards along the Z axis at a given speed
-            transform.Translate(0, 0, -speed * Time.deltaTime);
-        }
+        
+        transform.Translate(0, 0, -speed * Time.deltaTime);
 
-        // Check if marker's z position has reached the lower bound
-        if (transform.position.z <= trackTransport.lowerBound)
+        // Enable mesh renderers when we've passed enableRendererPosition
+        if (transform.position.z <= enableRendererPosition && transform.position.z > disableRendererPosition)
         {
-            // Deactivate the marker
-            gameObject.SetActive(false);
+            foreach (MeshRenderer renderer in childMeshRenderers)
+            {
+                renderer.enabled = true;
+            }
+        }
+        // Disable mesh renderers when we've passed disableRendererPosition
+        else if (transform.position.z <= disableRendererPosition)
+        {
+            foreach (MeshRenderer renderer in childMeshRenderers)
+            {
+                renderer.enabled = false;
+            }
         }
     }
 }
